f19efb31650154d52c5713c3bf2ec78e6e7886e2
mrabhishek393 <abhisheksngh393@gmail.com>
2021-04-17 13:40:46 +0530

    CSCAN added

--- Raw_Files/Disk_Scheduling/CSCAN.cpp
+++ Raw_Files/Disk_Scheduling/CSCAN.cpp
@@ -44,7 +44,8 @@ void cscanalgo::CSCAN(int n,int arr[], int head, string direction,int disk_size)
         //traversing to left
         for (int i = left.size() - 1; i >= 0; i--) {
             current = left[i];
-
+            auto it=std::find(sequence.begin(), sequence.end(), current);
+            if(it!=sequence.end()) continue;
             sequence.push_back(current);
             distance = abs(current - head);
             total_count += distance;
@@ -56,7 +57,8 @@ void cscanalgo::CSCAN(int n,int arr[], int head, string direction,int disk_size)
         //traversing to left
         for (int i = right.size()-1; i >=0 ; i--) {
                 current = right[i];
-
+                auto it=std::find(sequence.begin(), sequence.end(), current);
+                if(it!=sequence.end()) continue;
                 sequence.push_back(current);      //pushing the current request
 				distance = abs(current - head);   //calculating absolute difference between current request and previous R/W head position
 				total_count += distance;          //adding distance to total seek count
@@ -72,7 +74,8 @@ void cscanalgo::CSCAN(int n,int arr[], int head, string direction,int disk_size)
         //traversing to right
         for (int i = 0; i < right.size(); i++) {
             current = right[i];
-
+            auto it=std::find(sequence.begin(), sequence.end(), current);
+            if(it!=sequence.end()) continue;
             sequence.push_back(current);      //pushing the current request
             distance = abs(current - head);   //calculating absolute difference between current request and previous R/W head position
             total_count += distance;          //adding distance to total seek count
@@ -84,7 +87,8 @@ void cscanalgo::CSCAN(int n,int arr[], int head, string direction,int disk_size)
         //traversing to right
         for (int i = 0; i < left.size(); i++) {
             current = left[i];
-
+            auto it=std::find(sequence.begin(), sequence.end(), current);
+            if(it!=sequence.end()) continue;
             sequence.push_back(current);
             distance = abs(current - head);
             total_count += distance;
@@ -109,20 +113,34 @@ void cscanalgo::CSCAN(int n,int arr[], int head, string direction,int disk_size)
 void cscanalgo::solve()
 {
     int n,head,choice,disk_size;
-	cout<<"Enter the number of requests: ";
+	cout<<"Enter the number of pages: ";
 	cin>>n;
 	int arr[n];
 
-    cout<<"\nEnter the size of the disk: ";
+    cout<<"\nEnter the total number of tracks in secondary memory : ";
     cin>>disk_size;
     cout<<"\n";
 	for(int i=0;i<n;i++)
     {
-        cout<<"Enter the position of Request "<<i+1<<": ";
+        cout<<"Enter the track location of Page "<<i+1<<": ";
         cin>>arr[i];
+        if(arr[i]>=disk_size || arr[i]<0)
+        {
+            cout<<"Invalid Track location entered\n\n";
+            i--;
+        }
     }
-    cout<<"\nEnter the initial position of Read/Write Head: ";
-	cin>> head;
+    while(1)
+    {
+      cout<<"\nEnter the initial position of Read/Write Head: ";
+	  cin>> head;
+	  if(head>=disk_size || head<0)
+	  {
+	      cout<<"Invalid Head location entered\n\n";
+      }
+      else break;
+    }
+
     cout<<"\nSelect the initial direction of Read/Write Head: 1) Left 2) Right: ";
 	cin>>choice;
 	string direction="right";
