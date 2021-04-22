26af2f175469eac621cb4b583a61833fbfe34812
Anirudh Achal <anirudh.achal@gmail.com>
2021-04-23 03:06:49 +0530

    Codes Merged

--- main.cpp
+++ main.cpp
@@ -13,8 +13,8 @@
 #include "2. Process_Synchronization/Producer_Consumer.h"
 #include "2. Process_Synchronization/Readers_priority.h"
 #include "2. Process_Synchronization/Writers_priority.h"
-#include "2. Process_Synchronization/dining_philosopher.h"
-#include "2. Process_Synchronization/sleeping_barber.h"
+//#include "2. Process_Synchronization/dining_philosopher.h"
+//#include "2. Process_Synchronization/sleeping_barber.h"
 
 
 // Resource Allocation Deadlock
@@ -76,8 +76,8 @@ void print_menu_process_synchronization()
     cout << "1. Producer Consumer\n";
     cout << "2. Readers Priority\n";
     cout << "3. Writers Priority\n";
-    cout << "4. Dining Philosopher\n";
-    cout << "5. Sleeping Barber\n";
+    //cout << "4. Dining Philosopher\n";
+    //cout << "5. Sleeping Barber\n";
     cout << "Enter any other key to return to main menu\n\n";
 }
 
@@ -163,8 +163,8 @@ int main()
             if (choice == 1) producer_consumer();
             else if (choice == 2) readers_priority();
             else if (choice == 3) writers_priority();
-            else if (choice == 4) dining_philosopher();
-            else if (choice == 5) sleeping_barber();
+            //else if (choice == 4) dining_philosopher();
+            // else if (choice == 5) sleeping_barber();
             else continue;
         }
         else if(choice == 3)
